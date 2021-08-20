f51c05457e825d46c26971412367bb28a00aab43
Grimm <Grimmjow_Jay@163.com>
2021-08-21 00:37:05 +0800

    xiaoxiami

---
+++ src/main/java/xiaoxiami/SecretPassword.java
@@ -0,0 +1,16 @@
+package xiaoxiami;
+
+/**
+ * @author Grimm
+ * @date 2021/8/20
+ */
+public class SecretPassword {
+
+    public static final String PASSWORD = "The secret of my heart. ";
+
+    public static final String ENCRYPT_SIGN = "_encrypt";
+
+    private SecretPassword() {
+    }
+
+}
