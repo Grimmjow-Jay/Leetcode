659229f37cf2b4efb17ef64fbd4cab6d2249e67b
JayYang <Jay_yang@aliyun.com>
2021-12-13 09:24:47 +0800

    911. 在线选举

--- src/main/java/license/MaximumDepthOfNAryTree.java
+++ src/main/java/license/MaximumDepthOfNAryTree.java
@@ -35,21 +35,21 @@ public class MaximumDepthOfNAryTree {
         }
         return maxChildDepth + 1;
     }
+    static class Node {
+        public int val;
+        public List<Node> children;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, List<Node> _children) {
+            val = _val;
+            children = _children;
+        }
+    }
 }
 
-class Node {
-    public int val;
-    public List<Node> children;
-
-    public Node() {
-    }
-
-    public Node(int _val) {
-        val = _val;
-    }
-
-    public Node(int _val, List<Node> _children) {
-        val = _val;
-        children = _children;
-    }
-}
