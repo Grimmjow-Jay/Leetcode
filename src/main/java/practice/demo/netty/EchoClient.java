14b3bddf86fd096b289621afadf22a9fb08ca6e2
JayYang <Jay_yang@aliyun.com>
2022-01-25 16:21:44 +0800

    netty nio demo

---
+++ src/main/java/practice/demo/netty/EchoClient.java
@@ -0,0 +1,53 @@
+package practice.demo.netty;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import lombok.RequiredArgsConstructor;
+
+import java.net.InetSocketAddress;
+
+/**
+ * @author Jay Yang
+ * @date 2022/1/25
+ */
+@RequiredArgsConstructor
+public class EchoClient {
+
+    private final String host;
+    private final int port;
+
+    public static void main(String[] args) throws Exception {
+
+        new EchoClient("localhost", 4999).start();
+
+    }
+
+    public void start() throws Exception {
+        EventLoopGroup group = new NioEventLoopGroup();
+
+        try {
+            Bootstrap bootstrap = new Bootstrap()
+                    .group(group)
+                    .channel(NioSocketChannel.class)
+                    .remoteAddress(new InetSocketAddress(host, port))
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) {
+                            ch.pipeline().addLast(new EchoClientHandler());
+                        }
+                    });
+            ChannelFuture channelFuture = bootstrap.connect().sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+
+    }
+
+
+}
---
+++ src/main/java/practice/demo/netty/NettyOioServer.java
@@ -0,0 +1,57 @@
+package practice.demo.netty;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.*;
+import io.netty.channel.oio.OioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.oio.OioServerSocketChannel;
+
+import java.nio.charset.StandardCharsets;
+
+/**
+ * @author Jay Yang
+ * @date 2022/1/25
+ */
+public class NettyOioServer {
+
+    public static void main(String[] args) throws Exception {
+        new NettyOioServer()
+                .server(4998);
+    }
+
+    public void server(int port) throws Exception {
+        ByteBuf buf = Unpooled.unreleasableBuffer(
+                Unpooled.copiedBuffer("Hi!\r\n", StandardCharsets.UTF_8));
+        OioEventLoopGroup group = new OioEventLoopGroup();
+
+        try {
+            ServerBootstrap serverBootstrap = new ServerBootstrap()
+                    .group(group)
+                    .channel(OioServerSocketChannel.class)
+                    .localAddress(port)
+                    .childHandler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        protected void initChannel(SocketChannel ch) {
+                            ch.pipeline()
+                                    .addLast(
+                                            new ChannelInboundHandlerAdapter() {
+                                                @Override
+                                                public void channelActive(ChannelHandlerContext ctx) {
+                                                    ctx.writeAndFlush(buf.duplicate())
+                                                            .addListener(ChannelFutureListener.CLOSE);
+                                                }
+                                            }
+                                    );
+                        }
+                    });
+            ChannelFuture channelFuture = serverBootstrap.bind().sync();
+            channelFuture.channel().closeFuture().sync();
+        } finally {
+            group.shutdownGracefully().sync();
+        }
+
+    }
+
+}
