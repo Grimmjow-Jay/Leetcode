8bf74c89326fda2f95efce84eaa36ac93b16a446
JayYang <Jay_yang@aliyun.com>
2022-05-18 14:14:40 +0800

    ProducerAndConsumerDemo

---
+++ src/main/java/practice/demo/ProducerAndConsumerDemo.java
@@ -0,0 +1,47 @@
+package practice.demo;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * @author Jay Yang
+ * @date 2022/5/18
+ */
+public class ProducerAndConsumerDemo {
+
+    public static void main(String[] args) {
+        Queue<String> logs = new LinkedList<>();
+        Thread consumer = new Thread(() -> {
+            while (true) {
+                String log = logs.poll();
+                if (log == null) {
+                    try {
+                        LockSupport.park();
+                        System.out.println("消费者被唤醒");
+                    } catch (Exception e) {
+                        return;
+                    }
+                } else {
+                    System.out.println("打印: " + log);
+                }
+            }
+        });
+        consumer.start();
+
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                String line = scanner.nextLine();
+                if ("ok".equals(line)) {
+                    System.exit(1);
+                    return;
+                } else {
+                    logs.add(line);
+                    LockSupport.unpark(consumer);
+                }
+            }
+        }
+    }
+
+}
