a7ff321aeed6a4a7f94f308b8e6d6bcee4e1441c
JayYang <Jay_yang@aliyun.com>
2022-10-10 14:18:19 +0800

    LongToIntDemo

---
+++ src/main/java/practice/demo/LongToIntDemo.java
@@ -0,0 +1,16 @@
+package practice.demo;
+
+/**
+ * @author Jay Yang
+ * @date 2022/10/10
+ */
+public class LongToIntDemo {
+
+    public static void main(String[] args) {
+
+        long l = (long) Integer.MAX_VALUE + 10L;
+        System.out.println((int) l);
+
+    }
+
+}
