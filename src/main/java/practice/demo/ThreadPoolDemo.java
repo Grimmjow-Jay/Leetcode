4a39b7a6fbabf0f0a071817234fddafa89b9073f
JayYang <Jay_yang@aliyun.com>
2022-12-01 02:46:02 +0800

    ThreadPoolDemo

---
+++ src/main/java/practice/demo/ThreadPoolDemo.java
@@ -0,0 +1,45 @@
+package practice.demo;
+
+import java.util.concurrent.*;
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * @author Jay Yang
+ * @date 2022/11/30
+ */
+public class ThreadPoolDemo {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        int corePoolSize = 2;
+        int maximumPoolSize = 10;
+        int capacity = 15;
+        long keepAliveTime = 10L;
+
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+                corePoolSize,
+                maximumPoolSize,
+                keepAliveTime,
+                TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(capacity),
+                Executors.defaultThreadFactory(),
+                new ThreadPoolExecutor.CallerRunsPolicy());
+
+        int count = 200;
+        CountDownLatch cdl = new CountDownLatch(count);
+        for (int i = 0; i < count; i++) {
+            threadPoolExecutor.execute(() -> {
+
+                System.out.println(Thread.currentThread() + "----START----" + (System.currentTimeMillis() / 1000));
+                LockSupport.parkUntil(System.currentTimeMillis() + 3000L);
+                System.out.println(Thread.currentThread() + "-----END-----" + (System.currentTimeMillis() / 1000));
+                cdl.countDown();
+
+            });
+        }
+
+        cdl.await();
+        threadPoolExecutor.shutdown();
+    }
+
+}
