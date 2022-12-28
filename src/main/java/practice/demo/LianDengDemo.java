9937360b0b59cbf8a0f5b3d6b1780033486c2b24
JayYang <Jay_yang@aliyun.com>
2022-12-28 08:47:08 +0800

    LianDengDemo

---
+++ src/main/java/practice/demo/LianDengDemo.java
@@ -0,0 +1,20 @@
+package practice.demo;
+
+/**
+ * @author Jay Yang
+ * @date 2022/12/27
+ */
+public class LianDengDemo {
+
+    public static void main(String[] args) {
+
+        int a;
+        int b;
+
+        a = b = 2;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+
+}
