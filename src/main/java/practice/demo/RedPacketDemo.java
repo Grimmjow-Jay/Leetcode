bd734d901b10a34b9dec0511ca5671eb37336d95
JayYang <Jay_yang@aliyun.com>
2023-01-03 11:42:38 +0800

    RedPacketDemo

--- src/main/java/practice/demo/RedPacketDemo.java
+++ src/main/java/practice/demo/RedPacketDemo.java
@@ -14,43 +14,51 @@ public class RedPacketDemo {
 
     public static void main(String[] args) {
 
-        int totalAmount = 867;
-        int packets = 10;
+        int totalAmount = 8610;
+        int packets = 20;
 
         Map<Integer, Long> doubleAverageStatistics = new TreeMap<>(Integer::compareTo);
         Map<Integer, Long> segmentCutStatistics = new TreeMap<>(Integer::compareTo);
-        int rounds = 200000;
-        for (int i = 0; i < rounds; i++) {
+        int rounds = 20000;
 
+        long start1 = System.currentTimeMillis();
+        for (int i = 0; i < rounds; i++) {
             AbstractRedPacket doubleAverageRedPacket = new DoubleAverageRedPacket(totalAmount, packets);
-            AbstractRedPacket segmentCutRedPacket = new SegmentCutRedPacket(totalAmount, packets);
             for (int j = 0; j < packets; j++) {
                 int doubleAverageRedPacketAmount = doubleAverageRedPacket.open();
                 Long doubleAverageTotalAmount = doubleAverageStatistics.get(j);
                 doubleAverageTotalAmount = doubleAverageTotalAmount == null ? doubleAverageRedPacketAmount : (doubleAverageTotalAmount + doubleAverageRedPacketAmount);
                 doubleAverageStatistics.put(j, doubleAverageTotalAmount);
+            }
+        }
+        long end1 = System.currentTimeMillis();
 
+        long start2 = System.currentTimeMillis();
+        for (int i = 0; i < rounds; i++) {
+            AbstractRedPacket segmentCutRedPacket = new SegmentCutRedPacket(totalAmount, packets);
+            for (int j = 0; j < packets; j++) {
                 int segmentCutRedPacketAmount = segmentCutRedPacket.open();
                 Long segmentCutTotalAmount = segmentCutStatistics.get(j);
                 segmentCutTotalAmount = segmentCutTotalAmount == null ? segmentCutRedPacketAmount : (segmentCutTotalAmount + segmentCutRedPacketAmount);
                 segmentCutStatistics.put(j, segmentCutTotalAmount);
-
-
             }
-
         }
+        long end2 = System.currentTimeMillis();
 
         for (int i = 0; i < packets; i++) {
             Long doubleAverageAmount = doubleAverageStatistics.get(i);
             Long segmentCutAmount = segmentCutStatistics.get(i);
-            System.out.println(i + "\t\tdoubleAverage:" + doubleAverageAmount + "\t\tsegmentCut:" + segmentCutAmount);
+            System.out.println(i + "\tdoubleAverage:\t" + doubleAverageAmount + "\tsegmentCut:\t" + segmentCutAmount);
         }
+
+        System.out.println(end1 - start1);
+        System.out.println(end2 - start2);
 
     }
 
     private static abstract class AbstractRedPacket {
 
-        protected static final int MIN_AMOUNT = 1;
+        protected static final int MIN_AMOUNT = 2;
         protected static final Random RANDOM = ThreadLocalRandom.current();
 
         protected final int totalAmount;
@@ -70,7 +78,7 @@ public class RedPacketDemo {
                 throw new IllegalStateException("红包没有了");
             }
 
-            int amount = randomAmount();
+            int amount = openRedPacket();
 
             leftPackets--;
             leftAmount -= amount;
@@ -83,7 +91,7 @@ public class RedPacketDemo {
          *
          * @return amount
          */
-        protected abstract int randomAmount();
+        protected abstract int openRedPacket();
 
         public boolean openFinished() {
             return leftPackets <= 0;
@@ -98,7 +106,7 @@ public class RedPacketDemo {
         }
 
         @Override
-        public int randomAmount() {
+        public int openRedPacket() {
 
             if (leftPackets == 1) {
                 return leftAmount;
@@ -121,21 +129,24 @@ public class RedPacketDemo {
         }
 
         private int[] initIndices() {
-            int totalAmount = super.totalAmount - MIN_AMOUNT * packets;
-            int[] indices = new int[packets + 1];
-            for (int i = 0; i <= packets; i++) {
-                indices[i] = RANDOM.nextInt(totalAmount + MIN_AMOUNT);
+            int totalAmount = super.totalAmount - (MIN_AMOUNT * packets);
+            int[] indices = new int[packets - 1];
+            for (int i = 0; i < packets - 1; i++) {
+                indices[i] = RANDOM.nextInt(totalAmount + 1);
             }
             Arrays.sort(indices);
             return indices;
         }
 
         @Override
-        public int randomAmount() {
+        public int openRedPacket() {
             if (leftPackets <= 1) {
                 return leftAmount;
             }
-            return indices[leftPackets] - indices[leftPackets - 1] + MIN_AMOUNT;
+            if (leftPackets == packets) {
+                return super.totalAmount - (MIN_AMOUNT * packets) - indices[packets - 2] + MIN_AMOUNT;
+            }
+            return indices[leftPackets - 1] - indices[leftPackets - 2] + MIN_AMOUNT;
         }
 
     }
