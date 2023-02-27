3cb67c26b7f35bf202f46840dd182ae8b3a67f50
JayYang <Jay_yang@aliyun.com>
2023-02-27 09:10:10 +0800

    util: Pair

--- src/main/java/practice/util/Pair.java
+++ src/main/java/practice/util/Pair.java
@@ -1,25 +1,21 @@
 package practice.util;
 
+import lombok.Getter;
+
 import java.io.Serializable;
 import java.util.Objects;
 
 public class Pair<K, V> implements Serializable {
 
-    private K key;
+    @Getter
+    private final K key;
 
-    private V value;
+    @Getter
+    private final V value;
 
     public Pair(K key, V value) {
         this.key = key;
         this.value = value;
-    }
-
-    public K getKey() {
-        return key;
-    }
-
-    public V getValue() {
-        return value;
     }
 
     @Override
