3e7b73ea9093f997cc9422b43087ad112a849792
JayYang <Jay_yang@aliyun.com>
2023-11-08 11:06:45 +0800

    LocalDateToStringDemo

---
+++ src/main/java/practice/demo/LocalDateToStringDemo.java
@@ -0,0 +1,16 @@
+package practice.demo;
+
+import java.time.LocalDate;
+
+/**
+ * @author Jay Yang
+ * @date 2023/11/7
+ */
+public class LocalDateToStringDemo {
+
+    public static void main(String[] args) {
+        LocalDate date = LocalDate.of(2023, 9, 10);
+        System.out.println(date.toString());
+    }
+
+}
