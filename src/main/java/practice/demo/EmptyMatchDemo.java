6be835a98525b511c9a4f9aab93d4c58f2c4e0eb
JayYang <Jay_yang@aliyun.com>
2023-11-21 14:13:36 +0800

    EmptyMatchDemo

---
+++ src/main/java/practice/demo/EmptyMatchDemo.java
@@ -0,0 +1,27 @@
+package practice.demo;
+
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author Jay Yang
+ * @date 2023/11/21
+ */
+public class EmptyMatchDemo {
+
+    @SuppressWarnings("all")
+    public static void main(String[] args) {
+
+        List<String> emptyList = Collections.emptyList();
+
+        boolean allMatch = emptyList.stream()
+                .allMatch(e -> e.equals("a"));
+        System.out.println(allMatch);
+
+        boolean anyMatch = emptyList.stream()
+                .anyMatch(e -> e.equals("a"));
+        System.out.println(anyMatch);
+
+    }
+
+}
