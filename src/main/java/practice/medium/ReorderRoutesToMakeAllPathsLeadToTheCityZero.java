e24019063136f2af686ecfd080b711131c7a61cc
JayYang <Jay_yang@aliyun.com>
2023-12-07 13:33:51 +0800

    重新规划路线

---
+++ src/main/java/practice/medium/ReorderRoutesToMakeAllPathsLeadToTheCityZero.java
@@ -0,0 +1,52 @@
+package practice.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <pre>
+ * n 座城市，从 0 到 n-1 编号，其间共有 n-1 条路线。因此，要想在两座不同城市之间旅行只有唯一一条路线可供选择（路线网形成一颗树）。
+ * 去年，交通运输部决定重新规划路线，以改变交通拥堵的状况。
+ * 路线用 connections 表示，其中 connections[i] = [a, b] 表示从城市 a 到 b 的一条有向路线。
+ * 今年，城市 0 将会举办一场大型比赛，很多游客都想前往城市 0 。
+ * 请你帮助重新规划路线方向，使每个城市都可以访问城市 0 。返回需要变更方向的最小路线数。
+ * 题目数据 保证 每个城市在重新规划路线方向后都能到达城市 0 。
+ * </pre>
+ *
+ * @author Jay Yang
+ * @date 2023/12/7
+ */
+public class ReorderRoutesToMakeAllPathsLeadToTheCityZero {
+
+    public static void main(String[] args) {
+        int n = 6;
+        int[][] connections = {{0, 1}, {1, 3}, {2, 3}, {4, 0}, {4, 5}};
+        int minReorder = minReorder(n, connections);
+        System.out.println(minReorder);
+    }
+
+    private static int minReorder(int n, int[][] connections) {
+        @SuppressWarnings("unchecked")
+        List<int[]>[] e = new ArrayList[n];
+        for (int i = 0; i < n; i++) {
+            e[i] = new ArrayList<>();
+        }
+        for (int[] edge : connections) {
+            e[edge[0]].add(new int[]{edge[1], 1});
+            e[edge[1]].add(new int[]{edge[0], 0});
+        }
+        return dfs(0, -1, e);
+    }
+
+    private static int dfs(int x, int parent, List<int[]>[] e) {
+        int res = 0;
+        for (int[] edge : e[x]) {
+            if (edge[0] == parent) {
+                continue;
+            }
+            res += edge[1] + dfs(edge[0], x, e);
+        }
+        return res;
+    }
+
+}
